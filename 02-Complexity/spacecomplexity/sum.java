953cfd7628a251ad1c99e503b847be2a4446f1e8
RajeevKumar <rajeevjewar2@gmail.com>
2023-03-26 12:30:41 +0530

    second method to add sum of numbers till n

--- 02-Complexity/spacecomplexity/sum.java
+++ 02-Complexity/spacecomplexity/sum.java
@@ -2,10 +2,19 @@ package spacecomplexity;
 
 public class sum {
   public static void main(String[] args) {
-    System.out.println(sum(10));
+    System.out.println(getSum(10));
   }
-  static int sum(int n){
-    
-    return n*(n+1)/2;
+  // static int sum(int n){
+  //   return n*(n+1)/2;
+  // }
+// Space complexity of above solution is O(1).
+  // Other way to do sum.
+  static int getSum(int n){
+    int sum = 0;
+    for(int i = 0; i <= n; i++){
+      sum +=i;
+    }
+    return sum;
   }
+  // Space complexity of above solution is O(1).
 }
