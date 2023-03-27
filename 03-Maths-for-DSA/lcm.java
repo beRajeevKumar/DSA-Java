16343951ea5c7d9836a7b8994b71d5c56292b484
RajeevKumar <rajeevjewar2@gmail.com>
2023-03-27 19:21:36 +0530

    Lcm and Gcd problems are done

---
+++ 03-Maths-for-DSA/lcm.java
@@ -0,0 +1,28 @@
+public class lcm {
+  public static void main(String[] args) {
+// System.out.println(lcmFinder(4, 6));
+System.out.println(lcmBest(4, 6));
+  }
+  // static int lcmFinder(int a,int b){
+  //   int res=Math.max(a,b);
+  //   while(true){
+  //     if(res%a==0 && res%b==0){
+  //       return res;
+  //     } 
+  //     res++;
+  //   }
+  // }
+// The Time complexity of this solution is O(a*b-max(a,b)).
+
+// Efficient Soution 
+
+static int gcd(int a, int b){
+  if(b==0) return a;
+  return gcd(b,a%b);
+}
+static int lcmBest(int a, int b){
+
+  return (a*b)/gcd(a,b);
+}
+// The Time complexity of our function is O(log(min(a,b)))
+}
