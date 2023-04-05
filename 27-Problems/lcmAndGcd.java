06605f0c670d7fd4df1fac2d901b8c5393c14cd3
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-04-05 23:59:30 +0530

    LCM and GCD problem is solved on gfg

---
+++ 27-Problems/lcmAndGcd.java
@@ -0,0 +1,28 @@
+public class lcmAndGcd {
+  public static void main(String[] args) {
+    System.out.println(lcm(100, 200));
+  }
+
+  static Long[] lcmAndGcd(Long A, Long B) {
+    Long arr[] = new Long[2];
+    arr[0] = lcm(A, B);
+    arr[1] = gcd(A, B);
+    return arr;
+  }
+
+  static long gcd(long A, long B) {
+    if (B == 0)
+      return A;
+    return gcd(B, A % B);
+  }
+
+  static long lcm(long A, long B) {
+    return (A * B) / gcd(A, B);
+  }
+}
+// It seems to be print java Gargen in vs code terminal But in GFG Platform its
+// working perfectly.
+// Here is the link of the Problem:--
+//
+
+// https://practice.geeksforgeeks.org/problems/lcm-and-gcd4516/1?page=2&difficulty[]=-1&category[]=Arrays&category[]=Mathematical&sortBy=submissions
