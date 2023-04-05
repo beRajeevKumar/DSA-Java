76c5d785ebecf829e8f8a7c202ff0944a76e91b5
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-04-06 00:13:31 +0530

    First Java Program is solved in form Gfg course

---
+++ 01-Basic-Java-Problems/Basic-Programs/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args) {
+    int num;
+    System.out.println("Enter the Integer: ");
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    System.out.println("The Entered Number is: " + num);
+  }
+}
