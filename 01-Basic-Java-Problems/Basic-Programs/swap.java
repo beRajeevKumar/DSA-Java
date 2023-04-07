d018fc26b1e5c3da0cc835ed40f836e99740c0f4
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-04-07 21:41:07 +0530

    swap tow numbers problem is solve by another method in java

--- 01-Basic-Java-Problems/Basic-Programs/swap.java
+++ 01-Basic-Java-Problems/Basic-Programs/swap.java
@@ -3,9 +3,16 @@ public class swap {
     int num1 = 10;
     int num2 = 20;
     System.out.println("Before Swap: " + num1 + " " + num2);
-    int temp = num1;
-    num1 = num2;
-    num2 = temp;
+    // This method is about swap two numbers but taking any temp variable
+    // int temp = num1;
+    // num1 = num2;
+    // num2 = temp;
+    // System.out.println("After Swap: " + num1 + " " + num2);
+
+    // This method is about swap two numbers but not taking any temp variable
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
     System.out.println("After Swap: " + num1 + " " + num2);
   }
 }
