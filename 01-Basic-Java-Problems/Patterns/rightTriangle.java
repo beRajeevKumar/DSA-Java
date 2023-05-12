f8e474f2e43b1f44eafb1c144d3adf89973a5093
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-05-12 12:01:17 +0530

    Right Angle triangle problem is dynamic now

--- 01-Basic-Java-Problems/Patterns/rightTriangle.java
+++ 01-Basic-Java-Problems/Patterns/rightTriangle.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class rightTriangle {
   public static void main(String[] args) {
-
-    for (int i = 1; i <= 5; i++) {
+    System.out.println("Enter the value of N to print Right Triangle");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= i; j++) {
-        System.out.print("*");
+        System.out.print("* ");
       }
       System.out.println();
     }
   }
 }
+// We also can solve this problem by using recursion
