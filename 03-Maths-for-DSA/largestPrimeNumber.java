ea251f8bc97890ca60f458ec0346b9b4840ba6b0
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-07-24 10:27:20 +0530

    largest Prime Number problem solved on gfg

---
+++ 03-Maths-for-DSA/largestPrimeNumber.java
@@ -0,0 +1,17 @@
+public class largestPrimeNumber {
+  public static void main(String[] args) {
+    System.out.println(largestPrimeFactor(24));
+  }
+
+  static long largestPrimeFactor(int N) {
+    // code here
+    for (int i = 2; i * i <= N; i++) {
+      if (N % i == 0) {
+        while (N % i == 0 && N != i) {
+          N /= i;
+        }
+      }
+    }
+    return N;
+  }
+}
