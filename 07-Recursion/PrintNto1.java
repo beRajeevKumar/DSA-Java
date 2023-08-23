363572df851a3d6faae756599a5fd1c2af2208e3
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-08-23 17:29:20 +0530

    N to 1 is Print using Recursion

---
+++ 07-Recursion/PrintNto1.java
@@ -0,0 +1,14 @@
+public class PrintNto1 {
+  public static void main(String[] args) {
+    printNto1(10);
+  }
+
+  static void printNto1(int n) {
+    if (n == 0)
+      return;
+    System.out.print(n + " ");
+    printNto1(n - 1);
+  }
+  // The time complexity of this above function is Theta(n) and the auxillary
+  // complexity is Theta(n)
+}
