7ab283c25c3b90134f31d437b52f2749a0aba84d
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-08-23 20:03:40 +0530

    Factorial Problem is solved

---
+++ 07-Recursion/factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+  public static void main(String[] args) {
+    System.out.println(printFactorial(4));
+  }
+
+  static int printFactorial(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    return n * printFactorial(n - 1);
+  }
+}
