86481a919737cdecb23b857992769ec12a030c42
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-08-27 10:32:13 +0530

    Factorial Problem is solved by tail recursive method

--- 07-Recursion/factorial.java
+++ 07-Recursion/factorial.java
@@ -1,13 +1,23 @@
 public class factorial {
   public static void main(String[] args) {
-    System.out.println(printFactorial(4));
+    // System.out.println(printFactorial(4));
+    System.out.println(factTR(5, 1));
   }
 
-  static int printFactorial(int n) {
+  // static int printFactorial(int n) {
+  // if (n == 0) {
+  // return 1;
+  // }
+  // return n * printFactorial(n - 1);
+  // }
+
+  // Solving This by using Tail Recursion
+
+  static int factTR(int n, int a) {
     if (n == 0) {
-      return 1;
+      return a;
     }
-    return n * printFactorial(n - 1);
+    return factTR(n - 1, n * a);
   }
   // The recurrence relation of this function is T(n) = T(n - 1) + O(1)
   // Time complexity of the above function is Theta(n) and
