8c5db6ee3022f299153bdd1172935c4c1496be49
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-09-06 00:24:24 +0530

    Sum of AP Problem is solved

--- 01-Basic-Problems/SumOfAP.java
+++ 01-Basic-Problems/SumOfAP.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class SumOfAP {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Nth Term of AP: ");
     int n = sc.nextInt();
+    System.out.print("Enter First Term of AP: ");
     int a = sc.nextInt();
+    System.out.print("Enter Common Difference of AP: ");
     int d = sc.nextInt();
     int sum = 0;
     int nextTerm = a;
