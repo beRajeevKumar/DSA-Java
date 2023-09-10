831ffd5295cdb6684b7de791e8cefb73996b639d
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-09-10 21:29:55 +0530

    Middle Problem is solving

---
+++ 01-Basic-Problems/MiddleTerm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MiddleTerm {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Number 1: ");
+    int num1 = sc.nextInt();
+    System.out.print("Enter Number 2: ");
+    int num2 = sc.nextInt();
+    System.out.print("Enter Number 3: ");
+    int num3 = sc.nextInt();
+    sc.close();
+  }
+}
