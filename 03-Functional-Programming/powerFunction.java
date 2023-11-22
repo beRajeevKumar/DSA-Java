783d981df250cda3896a9766cd31e0b1c109e823
Rajeev Kumar <rajeevjewar2@gmail.com>
2023-11-22 22:48:12 +0530

    Functional Programming complete

--- 03-Functional-Programming/powerFunction.java
+++ 03-Functional-Programming/powerFunction.java
@@ -1,17 +1,20 @@
-package com.rajeev;
+
 import java.util.Scanner;
-public class powerFunction {
-    public static void powerFunction(int b,int p){
-        int value=1;
-        for(int i=p; i>=1;i--){
-             value=value*b;
+
+public class PowerFunction {
+// Functional Programming is all about to create seperate functions of repetative tasks.
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base = sc.nextInt();
+        int power = sc.nextInt();
+        calculatePower(base, power);
+        sc.close();
+    }
+    public static void calculatePower(int b, int p) {
+        int value = 1;
+        for (int i = p; i >= 1; i--) {
+            value = value * b;
         }
         System.out.println(value);
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int base=sc.nextInt();
-        int power= sc.nextInt();
-        powerFunction(base,power);
     }
 }
