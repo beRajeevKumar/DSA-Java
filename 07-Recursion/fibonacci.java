776028d70d49d0975945d8eac662ebc8f94f915e
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-01-27 19:27:18 +0530

    Basic Problems are Added

--- 07-Recursion/fibonacci.java
+++
@@ -1,6 +0,0 @@
-public class fibonacci {
-  public static void main(String[] args) {
-
-  }
-
-}
--- 14-OOPS-in-Java/SmallerNumbersThanCurrent.java
+++
@@ -1,9 +0,0 @@
-package com.rajeev;
-
-public class SmallerNumbersThanCurrent {
-    public static void main(String[] args) {
-        int nums[]={8,1,2,2,3};
-
-    }
-}
-
