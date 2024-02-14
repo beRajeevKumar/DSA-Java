a2629a3b227d8823a122c8607dbc478bb077e248
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-02-14 18:06:14 +0530

    Count Digits problem is solved with 4 different approaches

---
+++ 05-Maths-for-DSA/countDigits.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class countDigits {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number to count the number of digits in it: ");
+    // int N = sc.nextInt();
+    // System.out.println(countDigits01(N));
+    // long N = sc.nextLong();
+    // System.out.println("Number of digits: " + countDigits02(N));
+
+    int N = sc.nextInt();
+    System.out.println("Number of digits: " + countDigits04(N));
+    sc.close();
+  }
+
+  // Approach 01:
+  static int countDigits01(int n) {
+    int count = 0;
+    while (n != 0) {
+      n = n / 10;
+      count++;
+    }
+    return count;
+  }
+
+  // Approach 02:
+  static int countDigits02(long n) {
+    return (int) Math.floor(Math.log10(n) + 1);
+  }
+
+  // Approach 03:
+  static int countDigits03(int n) {
+    String num = Integer.toString(n);
+    return num.length();
+  }
+
+  // Approach 04:
+  static int countDigits04(int n) {
+    if (n / 10 == 0)
+      return 1;
+    return 1 + countDigits04(n / 10);
+  }
+}
