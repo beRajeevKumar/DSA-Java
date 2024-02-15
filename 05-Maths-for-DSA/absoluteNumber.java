eab1d8e66fca5bd7c0d13d8721d464dbdabc83c1
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-02-15 10:31:57 +0530

    Absoulte Number problem is solved

---
+++ 05-Maths-for-DSA/absoluteNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class absoluteNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number to find its absolute value: ");
+    int N = sc.nextInt();
+    System.out.println("The absolute value of the number: " + absoluteNumber01(N));
+    sc.close();
+  }
+
+  // Approach 01:
+  static int absoluteNumber01(int N) {
+    if (N <= 0) {
+      return -1 * N;
+    }
+    return N;
+  }
+
+  // Approach 03:
+
+  static int absoluteNumber02(int N) {
+    return Math.abs(N);
+  }
+}
